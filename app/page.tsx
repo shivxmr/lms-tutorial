c0eece2a031f75260af3d2ac42bfb19247031c1f
Shivam Raj <shivamraj.me21@rvce.edu.in>
2024-02-10 15:11:08 +0530

    installation done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
+    <>
     <p className="text-3xl font-medium text-sky-700">Hello world</p>
+    <Button variant="destructive">Button</Button>
+    </>
   );
 }
