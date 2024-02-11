c0903da4ef3f9264f21178d4be28e24f3c018795
Shivam Raj <shivamraj.me21@rvce.edu.in>
2024-02-11 22:27:42 +0530

    dynamic layout (without any error)

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,46 +1,9 @@
-// import { auth } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-
-// import { db } from "@/lib/db";
-
-// import { DataTable } from "./_components/data-table";
-// import { columns } from "./_components/columns";
-
-// const CoursesPage = async () => {
-//   const { userId } = auth();
-
-//   if (!userId) {
-//     return redirect("/");
-//   }
-
-//   const courses = await db.course.findMany({
-//     where: {
-//       userId,
-//     },
-//     orderBy: {
-//       createdAt: "desc",
-//     },
-//   });
-
-//   return ( 
-//     <div className="p-6">
-//       <DataTable columns={columns} data={courses} />
-//     </div>
-//    );
-// }
- 
-// export default CoursesPage;
-
-
-
-
-
 const CoursesPage = () => {
-  return ( 
-    <div>
-      Courses page
-    </div>
-   );
+    return ( 
+        <div>
+            Courses page!
+        </div>
+     );
 }
  
 export default CoursesPage;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ const DashboardLayout = ({
             <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
                 <Navbar />
             </div>
-            <div className="hidden md:flex h-full w-56 flex-col"> 
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50"> 
                 <Sidebar />
             </div>
             <main className="md:pl-56 pt-[80px] h-full">
