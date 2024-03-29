0c0a5027ccd4d43435410c9b9cb7d48e20d4bba1
Vikhyathraj Shetty K <itsvikhyathraj@gmail.com>
2024-03-29 11:50:05 +0530

    updates

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -20,11 +20,11 @@ const ChapterIdPage = async ({
 }: {
   params: { courseId: string; chapterId: string };
 }) => {
-	const session = await getLocalSession();
-	const userId = session?.userId;
-	if (!userId) {
-		return redirect("/");
-	}
+  const session = await getLocalSession();
+  const userId = session?.userId;
+  if (!userId) {
+    return redirect("/");
+  }
 
   const { chapter, course, nextChapter, userProgress, purchase } =
     await getChapter({
@@ -100,14 +100,15 @@ const ChapterIdPage = async ({
           className="flex flex-col mx-auto px-10 mt-10 pb-20"
           style={{
             // maxHeight: "38rem",
-            border: "0.12rem solid #6d94e3",
+            border: "0.12rem solid #cfcfcf",
             borderRadius: "0.1rem",
             height: "auto",
             // backgroundColor: "#cfe5ff",
             backgroundColor: "white",
             padding: "2rem",
             margin: "1.75rem",
-            boxShadow: "0px 0px 20px 5px rgb(0 0 0 / 5%)",
+            boxShadow:
+              "0px 7px 20px 5px rgb(0 0 0 / 10%),inset 0px 0px 1px 0px rgba(0, 0, 0, 0.3)",
           }}
         >
           <div className="grid grid-cols-2 gap-x-5 mb-7">
@@ -131,18 +132,57 @@ const ChapterIdPage = async ({
                   borderRadius: "0.1rem",
                 }}
               >
+                <form className="max-w-md mx-auto mb-4">
+                  <label
+                    // for="default-search"
+                    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                  >
+                    Search
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"></div>
+                    <input
+                      type="search"
+                      id="default-search"
+                      className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Search"
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      <svg
+                        className="w-4 h-3 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          stroke="white"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </form>
+
                 {YoutubeTranscript.fetchTranscript(chapter.videoUrl || "").then(
                   (res) => (
                     <>
                       {res.map((response) => (
                         <div
-                          className="flex border-solid border-y border-blue-50 hover:border-blue-300 transition-all ease-out"
+                          className="flex border-solid border-y border-grey-50 hover:border-blue-400 transition-all ease-out"
                           key={response.offset}
                         >
                           <div
                             style={{
                               width: "5rem",
-                              padding: "0.1rem 0rem 0.1rem 0.5rem",
+                              padding: "0.1rem 0rem 0.1rem 0.8rem",
                             }}
                           >
                             <span className="text-gray font-medium font-bold">
@@ -178,13 +218,13 @@ const ChapterIdPage = async ({
           className="flex flex-col mx-auto px-10 mt-10 pb-20"
           style={{
             // maxHeight: "38rem",
-            border: "0.12rem solid #6d94e3",
+            border: "0.12rem solid #cfcfcf",
             borderRadius: "0.1rem",
             height: "auto",
             backgroundColor: "white",
             padding: "2rem",
             margin: "2rem 1.75rem 1.75rem 1.75rem",
-            boxShadow: "0px 0px 20px 5px rgb(0 0 0 / 5%)",
+            boxShadow: "0px 7px 20px 5px rgb(0 0 0 / 10%)",
           }}
         >
           <div
@@ -192,7 +232,7 @@ const ChapterIdPage = async ({
               backgroundColor: "white",
               borderRadius: "0.1rem",
               padding: "0.5rem",
-              boxShadow: "0px 0px 20px 5px rgb(0 0 0 / 5%)",
+              boxShadow: "0px 7px 20px 5px rgb(0 0 0 / 10%)",
             }}
           >
             {!!attachments.length && (
@@ -207,7 +247,7 @@ const ChapterIdPage = async ({
                         href={`/pdf/${attachment.url}`}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-blue-500 hover:underline text-pretty items-center justify-center"
+                        className="text-black hover:underline text-pretty items-center justify-center"
                       >
                         <div className="m-2 flex flex-col items-center justify-center text-wrap">
                           <Image
@@ -231,7 +271,7 @@ const ChapterIdPage = async ({
           className="flex flex-col mx-auto px-10 mt-10 pb-20"
           style={{
             // maxHeight: "38rem",
-            border: "0.12rem solid #6d94e3",
+            border: "0.12rem solid #cfcfcf",
             borderRadius: "0.1rem",
             height: "auto",
             backgroundColor: "white",
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
 "use client";
 
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from "@/components/providers/toaster-provider";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
 import { SessionProvider } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<>
-			<html lang="en">
-				<body className={inter.className}>
-					<ConfettiProvider />
-					<ToastProvider />
-					<SessionProvider>{children}</SessionProvider>
-				</body>
-			</html>
-		</>
-	);
+  return (
+    <>
+      <html lang="en">
+        <body
+        // className={inter.className}
+        >
+          <ConfettiProvider />
+          <ToastProvider />
+          <SessionProvider>{children}</SessionProvider>
+        </body>
+      </html>
+    </>
+  );
 }
